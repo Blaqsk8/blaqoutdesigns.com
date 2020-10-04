c101a643aad16bcf34cc23637e4e1ddc27759414
Blaqsk8 <sonofstilgar@gmail.com>
2020-10-04 18:33:19 -0400

    Update index.php

--- projects/messengerclone/index.php
+++ projects/messengerclone/index.php
@@ -1,10 +1,10 @@
 <?php
-  require_once "header.php";
+    require_once "header.php";
 
-  echo "<br><span class="main">Welcom to $appname,";
+    echo "<br><span class="main">Welcom to $appname,";
 
-  if ($loggedin) echo " $user, you are logged in.";
-  else           echo " please sign up and/or log in to join in.";
+    if ($loggedin) echo " $user, you are logged in.";
+    else           echo " please sign up and/or log in to join in.";
 ?>
 
     </span><br><br>
